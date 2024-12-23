430b631d2aa5174f8cce4e7b866dc97107d22d26
Kati <Kati@SOCKAYA>
2024-12-23 21:42:05 +0300

    Считывание переменных из файла

--- Пелменный_цех/Пелменный_цех.py
+++ Пелменный_цех/Пелменный_цех.py
@@ -4,3 +4,15 @@ import csv
 from os import read
 import re
 import math
+
+def import_file_csv():
+    Arr = []
+    with open("../data.csv", "r") as file:
+        reader = csv.reader(file, delimiter=";")
+        for row in reader:
+            Arr.append(row[0])
+
+    return Arr
+
+
+arr = import_file_csv()
