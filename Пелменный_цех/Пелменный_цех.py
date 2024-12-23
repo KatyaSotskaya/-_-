a9c68f9007bf42d6735166975de90920dcd528c1
Kati <Kati@SOCKAYA>
2024-12-23 21:44:30 +0300

    Объявление переменных

--- Пелменный_цех/Пелменный_цех.py
+++ Пелменный_цех/Пелменный_цех.py
@@ -28,3 +28,29 @@ def import_file_csv_name_arr():
 
 
 name_arr = import_file_csv_name_arr()
+
+def import_file_csv_name_arr():
+    Arr = []
+    with open("../data.csv", "r") as file:
+        reader = csv.reader(file, delimiter=";")
+        for row in reader:
+            Arr.append(row[1])
+
+    return Arr
+
+
+name_arr = import_file_csv_name_arr()
+
+
+Qdaily = float(arr[0])  # суточная выработка готовой продукции Qсут
+t = float(arr[1])  # продолжительность рабочей смены
+
+# I. Технологическая линия изготовления пельменей
+Rpa = float(arr[2])  # производительность пельменного автомата рпа
+
+# II. Технологическая линия подготовки теста.
+at = float(arr[3])  # массовая доля теста в готовой продукции   ат %
+Rtm = float(arr[4])  # производительность тестомесильной машины   ртм
+
+# III. Технологическая линия подготовки фарша.
+Rk = float(arr[5])  # производительность куттера рк
